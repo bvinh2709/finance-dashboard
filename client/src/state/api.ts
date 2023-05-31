34bfa78aa5d1507d821fe7d3d98c496c916113be
Vinh Nguyen <nguyenlebaovinh.27@gmail.com>
2023-05-31 15:01:52 -0500

    tapping on backend

---
+++ client/src/state/api.ts
@@ -0,0 +1,15 @@
+import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react"
+
+export const api = createApi({
+    baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL }),
+    reducerPath: "main",
+    tagTypes: ["Kpis"],
+    endpoints: (build) => ({
+        getKpis: build.query<void, void>({
+            query: () => "kpi/kpis/",
+            providesTags: ["Kpis"],
+        })
+    })
+})
+
+export const { useGetKpisQuery } = api
