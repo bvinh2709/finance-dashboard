4d6f8873acf7d45317d8b2829526a9c4889fb82e
Vinh Nguyen <nguyenlebaovinh.27@gmail.com>
2023-05-30 23:06:43 -0500

    brand added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { useMemo } from "react"
 import { themeSettings } from "./theme"
 import { Box, CssBaseline } from "@mui/material"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Navbar from "./scenes/navbar"
 
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), [])
@@ -15,6 +16,7 @@ function App() {
             <Box
               width="100%" height="100%" padding="1rem 2rem 4rem 2rem"
             >
+              <Navbar />
               <Routes>
                 <Route path="/" element={<div>dashboard page</div>} />
                 <Route path="/predictions" element={<div>predictions page</div>} />
